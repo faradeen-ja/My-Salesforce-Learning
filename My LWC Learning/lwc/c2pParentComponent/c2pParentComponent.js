b5f33025310aee0d44d28296f50a24584790a273
Faradeen Jami <93830144+faradeen-ja@users.noreply.github.com>
2022-11-26 23:23:42 -0600

    Update c2pParentComponent.js

--- My LWC Learning/lwc/c2pParentComponent/c2pParentComponent.js
+++ My LWC Learning/lwc/c2pParentComponent/c2pParentComponent.js
@@ -7,12 +7,12 @@ export default class C2pParentComponent extends LightningElement {
 
 
     modalHandler(){
-        this.showModal = true             // 🔳 by clicking on the btn it will go visible  ✅ parent to child action
+        this.showModal = true             // 🔳 by clicking on the btn it will go visible  ✅parent to child action
     }
 
     closeHandler(event){       
         this.msg = event.detial              // whenever the ok button gets 📞called it will call the ➡️html and html will call the ➡️onclose event and onclose will call the ➡️close handler and the close handler will 🆗✅receive the event and it will 🤩start show modal false 
-        this.showModal = false               // by clicking on the ok it will call the parent   child to parent action
+        this.showModal = false               // by clicking on the ok it will call the parent   ✅child to parent action
 
                                                 
     }
