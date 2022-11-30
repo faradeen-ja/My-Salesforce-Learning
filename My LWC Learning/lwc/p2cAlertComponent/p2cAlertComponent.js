46dd1769c93a21fa796100ee57466fa40332b4a0
Faradeen Jami <93830144+faradeen-ja@users.noreply.github.com>
2022-11-29 23:22:19 -0600

    Update p2cAlertComponent.js

--- My LWC Learning/lwc/p2cAlertComponent/p2cAlertComponent.js
+++ My LWC Learning/lwc/p2cAlertComponent/p2cAlertComponent.js
@@ -4,8 +4,8 @@ export default class P2cAlertComponent extends LightningElement {
 
  @api message         //🔤string
  @api cardHeading    // ↘️receiving string
- @api number   // receiving number
- @api isValid   // receiving boolean
+ @api number   // 🔢receiving number
+ @api isValid   //↘️ receiving boolean
 
 
 
